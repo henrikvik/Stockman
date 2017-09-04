60e08e945ae1f74969f95eed22e04aa42e496f65
Henrik Vik <henkvik@gmail.com>
2017-09-04 10:01:21 +0200

    create renderer

--- Graphics/include/Renderer.h
+++ Graphics/include/Renderer.h
@@ -1,7 +1,50 @@
 #pragma once
+#include <Windows.h>
+#include <vector>
+#include <d3d11.h>
+
+class Camera;
+class Mat4;
+
+union AnimationInfo
+{
+
+};
+
+struct RenderInfo
+{
+    bool render;
+    int meshId;
+    int materialId;
+    Mat4 * translation;	
+};
+
+
+struct GBuffer
+{
+    ID3D11RenderTargetView * diffuseSpec;   // Dr Dg Db S
+    ID3D11RenderTargetView * normalMat;     // Nx Ny Nz Mid
+    ID3D11RenderTargetView * position;      // Px Py Pz Py
+    ID3D11DepthStencilView * depth;
+    ID3D11ShaderResourceView * diffuseSpecView;
+    ID3D11ShaderResourceView * normalMatView;
+    ID3D11ShaderResourceView * positionView;
+    ID3D11ShaderResourceView * depthView;
+};
+
 
 class Renderer
 {
 public:
-	Renderer();
+    Renderer(ID3D11Device * device, ID3D11DeviceContext * deviceContext);
+    void render(Camera * camera);
+    void qeueuRender(RenderInfo * renderInfo);
+private:
+    ID3D11Device * device;
+    ID3D11DeviceContext * deviceContext;
+    std::vector<RenderInfo*> renderQueue;
+    GBuffer gbuffer;
+
+    void createGBuffer();
+    void draw(RenderInfo* renderInfo);
 };
--- Graphics/source/Renderer.cpp
+++ Graphics/source/Renderer.cpp
@@ -1,7 +1,59 @@
 #include "Renderer.h"
 #include <stdio.h>
+#include "ThrowIfFailed.h"
 
-Renderer::Renderer()
+Renderer::Renderer(ID3D11Device * device, ID3D11DeviceContext * deviceContext)
+    : device(device)
+    , deviceContext(deviceContext)
 {
-	printf("Hello World");
+    createGBuffer();
 }
+
+void Renderer::render(Camera * camera)
+{
+    /*
+    //setCamera(sun);
+    //setShader(shadow);
+    //setRenderTarget(shadowMap);
+    //for (RenderInfo info : renderQueue)
+    //{
+    //  draw(info);
+    //}
+
+    setCamera(camera);
+    setShader(deffered);
+    
+*/
+
+    deviceContext->OMSetRenderTargets(3, (ID3D11RenderTargetView * const *)&gbuffer, gbuffer.depth);
+    for (RenderInfo * info : renderQueue)
+    {
+        draw(info);
+    }   
+}
+
+void Renderer::createGBuffer()
+{
+    D3D11_TEXTURE2D_DESC textureDesc = { 0 };
+    textureDesc.Width = 1280;
+    textureDesc.Height = 720;
+    textureDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    textureDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+
+    ID3D11Texture2D * diffuseSpecTexture;
+    ID3D11Texture2D * normalMatTexture;
+    ID3D11Texture2D * positionTexture;
+
+    ThrowIfFailed(device->CreateTexture2D(&textureDesc, nullptr, &diffuseSpecTexture));
+    ThrowIfFailed(device->CreateTexture2D(&textureDesc, nullptr, &normalMatTexture));
+    ThrowIfFailed(device->CreateTexture2D(&textureDesc, nullptr, &positionTexture));
+    ThrowIfFailed(device->CreateRenderTargetView(diffuseSpecTexture, nullptr, &gbuffer.diffuseSpec));
+    ThrowIfFailed(device->CreateRenderTargetView(normalMatTexture, nullptr, &gbuffer.normalMat));
+    ThrowIfFailed(device->CreateRenderTargetView(positionTexture, nullptr, &gbuffer.position));
+}
+
+void Renderer::draw(RenderInfo * renderInfo)
+{
+
+}
+
