a2b93385a51b70445ff3254fa110e11d7a30120b
Henrik Vik <henkvik@gmail.com>
2017-09-04 13:37:35 +0200

    renderer

--- Graphics/include/Renderer.h
+++ Graphics/include/Renderer.h
@@ -2,9 +2,9 @@
 #include <Windows.h>
 #include <vector>
 #include <d3d11.h>
-
-class Camera;
-class Mat4;
+#include <SimpleMath.h>
+#include "../Camera.h"
+#include <unordered_map>
 
 union AnimationInfo
 {
@@ -16,7 +16,7 @@ struct RenderInfo
     bool render;
     int meshId;
     int materialId;
-    Mat4 * translation;	
+    DirectX::SimpleMath::Matrix translation;	
 };
 
 
@@ -32,6 +32,11 @@ struct GBuffer
     ID3D11ShaderResourceView * depthView;
 };
 
+struct InstanceData
+{
+    DirectX::SimpleMath::Matrix translation;
+};
+
 
 class Renderer
 {
@@ -42,9 +47,15 @@ public:
 private:
     ID3D11Device * device;
     ID3D11DeviceContext * deviceContext;
+
     std::vector<RenderInfo*> renderQueue;
+    typedef  std::unordered_map<int, std::vector<InstanceData>> InstanceQueue_t;
+    InstanceQueue_t instanceQueue;
     GBuffer gbuffer;
 
+    ID3D11Buffer *instanceBuffer;
+
     void createGBuffer();
-    void draw(RenderInfo* renderInfo);
+    void cull();
+    void draw();
 };
--- Graphics/source/Renderer.cpp
+++ Graphics/source/Renderer.cpp
@@ -25,11 +25,19 @@ void Renderer::render(Camera * camera)
     
 */
 
-    deviceContext->OMSetRenderTargets(3, (ID3D11RenderTargetView * const *)&gbuffer, gbuffer.depth);
-    for (RenderInfo * info : renderQueue)
-    {
-        draw(info);
-    }   
+    //deviceContext->PSSetConstantBuffers(0, 3, nullptr);
+    //deviceContext->OMSetRenderTargets(3, (ID3D11RenderTargetView * const *)&gbuffer, gbuffer.depth);
+    
+    deviceContext->VSSetConstantBuffers(0, 1, nullptr);
+
+    
+    cull();
+    draw();
+}
+
+void Renderer::qeueuRender(RenderInfo * renderInfo)
+{
+    renderQueue.push_back(renderInfo);
 }
 
 void Renderer::createGBuffer()
@@ -52,8 +60,39 @@ void Renderer::createGBuffer()
     ThrowIfFailed(device->CreateRenderTargetView(positionTexture, nullptr, &gbuffer.position));
 }
 
-void Renderer::draw(RenderInfo * renderInfo)
+void Renderer::cull()
 {
+    for (RenderInfo * info : renderQueue)
+    {
+        if (info->render)
+        {
+            instanceQueue[info->meshId].push_back({ info->translation });
+        }
+    }
+    renderQueue.clear();
+}
 
+void Renderer::draw()
+{
+    // Sort instance id from all meshes
+    D3D11_MAPPED_SUBRESOURCE data = { 0 };
+    deviceContext->Map(instanceBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &data);
+
+    DWORD offset = 0;
+    for (InstanceQueue_t::value_type & pair : instanceQueue)
+    {
+        memcpy((void*)((DWORD)data.pData + offset), &pair.second.begin(), sizeof(pair.second));
+        offset += sizeof(pair.second);
+    }
+    
+    deviceContext->Unmap(instanceBuffer, 0);
+
+
+    // draw all instanced meshes
+    for (InstanceQueue_t::value_type & pair : instanceQueue)
+    {
+        //getVertexBuffer(pair.first);
+        //deviceContext->DrawInstanced()
+    }
 }
 
