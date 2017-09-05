82404b947fc52fec41fc21aebfafea15146a4561
Markus Werdien <markuswerdien@hotmail.com>
2017-09-05 11:03:49 +0200

    Removed function

--- Graphics/include/Constants.h
+++ Graphics/include/Constants.h
@@ -16,24 +16,4 @@ namespace Graphics
         { -0.5, 0.5 },
         { 0.5, 0.5 }
     };
-
-    static  ID3D11Buffer * FSQuad;
-    static  ID3D11Buffer * Triangle;
-
-    void InitializeConstants(ID3D11Device * device)
-    {
-        D3D11_BUFFER_DESC bufferDesc = { 0 };
-        bufferDesc.ByteWidth = sizeof(FSQuadVerts);
-        bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-
-        D3D11_SUBRESOURCE_DATA data = { 0 };
-        data.pSysMem = FSQuadVerts;
-
-        device->CreateBuffer(&bufferDesc, &data, &FSQuad);
-
-        bufferDesc.ByteWidth = sizeof(TriangleVerts);
-        data.pSysMem = TriangleVerts;
-
-        device->CreateBuffer(&bufferDesc, &data, &Triangle);
-    }
 };
