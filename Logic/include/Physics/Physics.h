17587180ee9ac3c0d6e784b17840a94f877d3001
Simon <te11c.simon.fredholm@gmail.com>
2017-09-06 17:25:09 +0200

    Woooo

--- Logic/include/Physics/Physics.h
+++ Logic/include/Physics/Physics.h
@@ -2,20 +2,13 @@
 #define PHYSICS_H
 
 /* 
- * How to add rigid bodies into the world: 
-  1. Make a btRigidBody object
-  2. Set it up however you want
-  3. Call physics->addRigidBody(ptr to rigidbody)
-
-  (btRigidbody is derived from btCollisionObject)
-
+ - Add rigid bodies into the world: 
+  1. Make a btRigidBody object, add mass and stuff like that
+  2. Call physics->addRigidBody(ptr to rigidbody)
  */
 
-// #include "btBulletCollisionCommon.h"
-// #include "btBulletDynamicsCommon.h"
-
-#include <btBulletCollisionCommon.h>
-#include <btBulletDynamicsCommon.h>
+#include "btBulletCollisionCommon.h"
+#include "btBulletDynamicsCommon.h"
 
 #define PHYSICS_GRAVITY 9.82
 #define PHYSICS_TIME_STEP 1.f / 60.f
