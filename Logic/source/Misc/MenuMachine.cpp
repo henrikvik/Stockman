004b780009de7dd85fb487ed520a02d1e96dc310
Emanuel <Emanuel@DESKTOP-4NEGGB7>
2017-09-07 10:24:51 +0200

    added Enums.h, MenuMachine

---
+++ Logic/source/Misc/MenuMachine.cpp
@@ -0,0 +1,28 @@
+#include "Misc\MenuMachine.h"
+
+using namespace Logic;
+
+MenuMachine::MenuMachine()
+{
+}
+
+
+MenuMachine::~MenuMachine()
+{
+}
+
+void Logic::MenuMachine::showMenu(GameState state)
+{
+	
+}
+
+void Logic::MenuMachine::removeMenu()
+{
+}
+
+void Logic::MenuMachine::readMenuLayoutFromFile()
+{
+
+}
+
+
