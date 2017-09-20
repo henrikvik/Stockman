c88e9d0a81ff61a2eb3db1b6b643d24c3056658e
bjurrsson <emanuel.bjurman@gmail.com>
2017-09-20 11:23:02 +0200

    bigger shotgun projectiles

--- Logic/source/Player/Weapon/WeaponManager.cpp
+++ Logic/source/Player/Weapon/WeaponManager.cpp
@@ -69,7 +69,7 @@ void WeaponManager::initializeWeapons()
 	m_allWeapons =
 	{
 		{ m_projectileManager, ProjectileData(1, 1, 1, 100, 0, 3000, Graphics::ModelID::CUBE, 1), 1, 60, 60, 30, 30, 1, 1, 0, 0, 1, 450, 1, 2000 },
-		{ m_projectileManager, ProjectileData(1, 0.01f, 1, 100, 0, 500, Graphics::ModelID::CUBE, 1), 1, 20, 20, 8, 8, 6, 18, 15, 10, 1, 100, 1, 2000 },
+		{ m_projectileManager, ProjectileData(1, 0.1f, 1, 100, 0, 500, Graphics::ModelID::CUBE, 1), 1, 20, 20, 8, 8, 6, 18, 15, 10, 1, 100, 1, 2000 },
 		{ m_projectileManager, ProjectileData(), 1, 20, 20, 8, 8, 1, 1, 1, 1, 1, 1, 1, 3000 },
 		{ m_projectileManager, ProjectileData(), 1, 20, 20, 8, 8, 3, 1, 1, 1, 1, 1, 1, 3000 },
 		{ m_projectileManager, ProjectileData(), 1, 20, 20, 8, 8, 0, 1, 1, 1, 1, 1, 1, 3000 },
