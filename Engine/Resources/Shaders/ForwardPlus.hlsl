c52fbe10ba2a77e03c5f06a13dd4481d264c1225
Markus Werdien <markuswerdien@hotmail.com>
2017-09-22 17:24:36 +0200

    moved sampling to beginning for easier multiplications. Will restructure on monday.

--- Engine/Resources/Shaders/ForwardPlus.hlsl
+++ Engine/Resources/Shaders/ForwardPlus.hlsl
@@ -112,6 +112,8 @@ PSOutput PS(VSOutput input) {
 	uint2 tile = uint2(floor(input.pos.xy / BLOCK_SIZE));
 	uint offset = LightGrid[tile].x;
 	uint count = LightGrid[tile].y;
+
+    float3 textureColor = diffuseMap.Sample(Sampler, input.uv);
 	
 
     ///////////////////////////////DIRECTIONAL LIGHT///////////////////////////////////////
@@ -143,6 +145,12 @@ PSOutput PS(VSOutput input) {
 
     float shadow = addedShadow / pow(samples * 2 + 1, 2);
     
+
+        //DEBUG TEST TEMP
+    if (input.lightPos.x > 1 || input.lightPos.x < 0 ||
+        input.lightPos.y > 1 || input.lightPos.y < 0)
+        shadow = 1;
+
             //////////////////////// END SHADOW /////////////////////////
 
     float3 directionalComponent = (directionalDiffuse + specularDir) * dirFade * shadow;
@@ -150,18 +158,10 @@ PSOutput PS(VSOutput input) {
     /////////////////////////////DIRECTIONAL LIGHT END///////////////////////////////////////
 
 
-    float3 ambient = float3(0.1, 0.1, 0.2);
-
-
-
-    //This will include a texture sample or material color later
-	float3 matColor = 0.1f;
+    ////////////////////////////////POINT LIGHTS//////////////////////////////////////////////
 
     float3 pointLightComponent = 0;
 
-
-
-    ////////////////////////////////POINT LIGHTS//////////////////////////////////////////////
 	for (uint i = 0; i < count; i++) 
     {
 		uint idx = LightIndexList[offset + i];
@@ -178,46 +178,21 @@ PSOutput PS(VSOutput input) {
 
        
         float3 specular = pow(saturate(dot(input.normal, reflectThing)), 1000) * light.color;
+
+
         pointLightComponent += light.color * attenuation * (saturate(diffuse) + specular);
 	}
     //////////////////////////////POINT LIGHTS END//////////////////////////////////////////////
 
+    
+    float3 ambient = float3(0.3, 0.3, 0.4);
 
-     /////////////////////////////SHADOWS//////////////////////////////
-    input.lightPos.x = (input.lightPos.x * 0.5f) + 0.5f;
-    input.lightPos.y = (input.lightPos.y * -0.5f) + 0.5f;
-
-    float addedShadow = 0;
-
-    int samples = 1;
-
-    for (int y = -samples; y <= samples; y += 1)
-    {
-        for (int x = -samples; x <= samples; x += 1)
-        {
-            addedShadow += shadowMap.SampleCmp(cmpSampler, input.lightPos.xy, input.lightPos.z, int2(x, y)).r;
-        }
-    }
-
-	
+    float3 lighting = saturate(directionalComponent + pointLightComponent + ambient);
+    
 
 
-    float shadow = addedShadow / 9;
-    //////////////////////// END SHADOW /////////////////////////
 
-
-    output.color = float4(saturate(directionalComponent + pointLightComponent + ambient), 1);
-    float3 textureColor = diffuseMap.Sample(Sampler, input.uv);
-    output.color = float4(saturate((directionalDiffuse * shadow) + textureColor + ambient), 1);
-   
-
-    //DEBUG TEST TEMP
-    //if (input.lightPos.x > 1 || input.lightPos.x < 0 ||
-    //    input.lightPos.y > 1 || input.lightPos.y < 0)
-    //    output.color.xyz = ambient;
-
-        
-
+    output.color = float4(lighting * textureColor, 1);
 
 	return output;
 }
