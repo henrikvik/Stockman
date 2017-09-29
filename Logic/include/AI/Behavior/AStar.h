8394524113de131c35ea41ff52516462110c2da8
Lukas Westling <euluken@hotmail.com>
2017-09-29 09:48:44 +0200

    ITS ALIVE

--- Logic/include/AI/Behavior/AStar.h
+++ Logic/include/AI/Behavior/AStar.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 
 #include "NavigationMesh.h"
 #include "PASVF.h"
@@ -23,8 +24,8 @@ namespace Logic
 			// to calc path testing rn
 			struct NavNode
 			{
-				bool onClosedList; // for testing
-				int nodeIndex; // index in nav mesh
+				bool onClosedList, explored; // for testing
+				int nodeIndex, parent; // index in nav mesh
 				float h, g; // cost to node
 
 				// remove later, this is unused at the moment
@@ -50,14 +51,14 @@ namespace Logic
 			float heuristic(DirectX::SimpleMath::Vector3 &from,
 				DirectX::SimpleMath::Vector3 &to) const;
 			void generateNodesFromFile();
-			bool nodeInQue(int index, std::priority_queue<int> const &que) const;
+			bool nodeInQue(int index, std::priority_queue<NavNode*> que) const;
 		public:
 			// string for the offline loaded nav mesh
 			AStar(std::string file);
 			~AStar();
 
 			Node getNextNode(Entity const &enemy, Entity const &target);
-			void reconstructPath(std::vector<NavNode> &navNodes,
+			void reconstructPath(std::vector<NavNode*> &navNodes,
 				std::vector<DirectX::SimpleMath::Vector3> &nodes);
 			void renderNavigationMesh(Graphics::Renderer &renderer);
 
--- Logic/source/AI/Behavior/AStar.cpp
+++ Logic/source/AI/Behavior/AStar.cpp
@@ -23,13 +23,13 @@ AStar::Node AStar::getNextNode(Entity const &enemy, Entity const &target)
 	// reset nav nodes
 	for (int i = 0; i < navNodes.size(); i++)
 	{
-		navNodes[i].onClosedList = false;
+		navNodes[i].onClosedList = navNodes[i].explored = false;
 		navNodes[i].h = navNodes[i].g = 0;
+		navNodes[i].parent = 0;
 	}
 
 	// openlist and a test offset
-	auto comp = [](int a, int b) { return navNodes[a] > navNodes[b]; };
-	std::priority_queue<int> openList;
+	std::priority_queue<NavNode*> openList;
 	DirectX::SimpleMath::Vector3 offset(0, 5, 0);
 
 	// get indicies
@@ -42,41 +42,65 @@ AStar::Node AStar::getNextNode(Entity const &enemy, Entity const &target)
 		return { 0, target.getPositionBT() };
 
 	navNodes[startIndex].h = heuristic(nodes[startIndex], nodes[endIndex]);
-	openList.push(startIndex);
+	openList.push(&navNodes[startIndex]);
 
-	int currentNode;
+	NavNode *currentNode = nullptr;
+	NavNode *explore = nullptr;
+
 	float f;
+
 	while (!openList.empty())
 	{
 		currentNode = openList.top();
-		f = navNodes[currentNode].g + navNodes[currentNode].h;
+		f = currentNode->g + currentNode->h;
 		openList.pop();
 
-		for (int index : navigationMesh.getEdges(currentNode))
-			if (!navNodes[index].onClosedList)
+		for (int index : navigationMesh.getEdges(currentNode->nodeIndex))
+		{
+			explore = &navNodes[index];
+			if (!explore->explored) // Unexplored
 			{
-				navNodes[index].g = f;
-				navNodes[index].h = heuristic(nodes[index], nodes[endIndex]);
-				openList.push(index);
-			}
-			else if (nodeOnList(index, openList.))
-			{
+				explore->explored = true;
 
-			}
-			else if (navNodes[index].g < f)
-			{
-				navNodes[index].g = f;
-				openList.push(index);
-			}
+				explore->g = f;
+				explore->h = heuristic(nodes[index], nodes[endIndex]);
 
-		navNodes[currentNode].onClosedList = true;
+				explore->parent = currentNode->nodeIndex;
+				openList.push(explore);
+			}
+			else if (explore->explored && !explore->onClosedList && explore->g > f) // in Open List
+			{
+				explore->g = f;
+
+				explore->parent = currentNode->nodeIndex;
+			}
+			else if (explore->g > f) // in Closed List
+			{
+				explore->onClosedList = false;
+
+				explore->g = f;
+
+				explore->parent = currentNode->nodeIndex;
+				openList.push(explore);
+			}
+		}
+
+		printf("test loop");
+		currentNode->onClosedList = true;
 	}
 
-	DirectX::SimpleMath::Vector3 node = nodes[currentNode];
+	if (!currentNode)
+	{
+		printf("Major Warning: A* can't find path, enemy or player is in a bad location!\nContact"
+			"Lukas or something (AStar.cpp:%s)\n", __LINE__);
+		return { 0, {nodes[endIndex].x, nodes[endIndex].y, nodes[endIndex].z} };
+	}
+
+	DirectX::SimpleMath::Vector3 node = nodes[currentNode->nodeIndex];
 	return { 0, { node.x, node.y, node.z } };
 }
 
-void AStar::reconstructPath(std::vector<NavNode>& navNodes, std::vector<DirectX::SimpleMath::Vector3>& nodes)
+void AStar::reconstructPath(std::vector<NavNode*>& navNodes, std::vector<DirectX::SimpleMath::Vector3>& nodes)
 {
 }
 
@@ -97,8 +121,8 @@ void AStar::generateNavigationMesh()
 	navNodes.clear();
 	for (int i = 0; i < navigationMesh.getNodes().size(); i++)
 		navNodes.push_back(
-			{ true, i,
-			0, 0 }
+			{ false, false, 
+			i, 0, 0 }
 		);
 }
 
@@ -113,10 +137,13 @@ void AStar::generateNodesFromFile()
 
 }
 
-bool AStar::nodeInQue(int index, std::priority_queue<int> const &que) const
+// this can maybe be optimized record most used functions to see which is needed
+bool AStar::nodeInQue(int index, std::priority_queue<NavNode*> que) const
 {
-	for (int i = 0; i < que.size(); i++)
-		if (que[i] == index)
+	while (!que.empty())
+		if (que.top()->nodeIndex == index)
 			return true;
+		else
+			que.pop();
 	return false;
 }
