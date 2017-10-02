50acba673b2ee95e3875b3c59223a6e4d9d8c2c8
Fredholm <te11c.simon.fredholm@gmail.com>
2017-10-02 13:20:20 +0200

    temp alt for mouse

--- Logic/source/Player/Player.cpp
+++ Logic/source/Player/Player.cpp
@@ -135,8 +135,8 @@ int Player::getHP() const
 void Player::updateSpecific(float deltaTime)
 {
 	// Get Mouse and Keyboard states for this frame
-	DirectX::Mouse::Get().SetMode(DirectX::Mouse::MODE_RELATIVE);
 	DirectX::Keyboard::State ks = DirectX::Keyboard::Get().GetState();
+	DirectX::Mouse::Get().SetMode(ks.IsKeyDown(DirectX::Keyboard::LeftAlt) ? DirectX::Mouse::MODE_ABSOLUTE : DirectX::Mouse::MODE_RELATIVE);
 	DirectX::Mouse::State ms = DirectX::Mouse::Get().GetState();
 
 	// Temp for testing
