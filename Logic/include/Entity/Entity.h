bb77c9ff481ac5b8ec15c46febbd9aa8fbd68b55
Fredholm <te11c.simon.fredholm@gmail.com>
2017-10-03 16:51:51 +0200

    removed some stuff

--- Logic/include/Entity/Entity.h
+++ Logic/include/Entity/Entity.h
@@ -44,57 +44,6 @@ namespace Logic
 		btRigidBody* m_body;
 		btTransform* m_transform;
 	};
-
-	//class TestHeadShot : public Entity
-	//{
-	//public:
-	//	TestHeadShot(btRigidBody* body, btVector3 halfExtent, Graphics::ModelID modelID = Graphics::ModelID::CUBE) : Entity(body, halfExtent, modelID) 
-	//	{
-	//		// Debug draw ray
-	//		renderDebug.points = new std::vector<DirectX::SimpleMath::Vector3>;
-	//		renderDebug.color = DirectX::SimpleMath::Color(1, 1, 1);
-	//		renderDebug.topology = D3D11_PRIMITIVE_TOPOLOGY_LINELIST;
-	//		renderDebug.useDepth = true;
-	//	}
-	//	
-	//	~TestHeadShot() { delete renderDebug.points; }
-
-	//	void updateSpecific(float deltaTime) { }
-	//	void onCollision(Entity& other) { }
-	//	void render(Graphics::Renderer& renderer)
-	//	{
-	////		Object::render(renderer);
-
-	//		renderDebug.points->clear();
-
-	//		btRigidBody* rb = this->getRigidbody();
-	//		btVector3 dimensions = this->getHalfExtent();
-	//		btVector3 position = this->getPositionBT();
-
-	//		btCollisionShape* cs = rb->getCollisionShape();
-	//		btBoxShape* bs = dynamic_cast<btBoxShape*>(cs);
-
-	//		btVector3 vp = {0, 0, 0};
-	//		
-	//		for (int i = 0; i < bs->getNumVertices(); i++)
-	//		{
-	//			for (int j = 0; j < bs->getNumVertices(); j++)
-	//			{
-	//				bs->getVertex(i, vp);
-	//				renderDebug.points->push_back(DirectX::SimpleMath::Vector3(position + vp));
-	//				bs->getVertex(j, vp);
-	//				renderDebug.points->push_back(DirectX::SimpleMath::Vector3(position + vp));
-	//			}
-	//		}
-	//			head->setVo
-	//		renderer.queueRenderDebug(&renderDebug);
-	//	}
-	//	
-
-	//private:
-	//	btRigidBody* head;
-	//	Graphics::RenderDebugInfo renderDebug;
-	//};
 }
 
 #endif // !ENTITY_H
