5e3e3e7531bc4e0b46ea6611a8d19325ad5c22d8
jakob nyberg <armas.94@hotmail.com>
2017-10-03 15:32:18 +0200

    removed test text

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -350,9 +350,9 @@ int Engine::run()
         {
             renderer->queueRender(&staticCube);
             renderer->queueRender(&staticSphere);
-            renderer->queueText(&text);
+         /*   renderer->queueText(&text);
             renderer->queueText(&text1);
-            renderer->queueText(&text2);
+            renderer->queueText(&text2);*/
 			PROFILE_BEGINC("Renderer::render()", EventColor::PinkDark);
             renderer->render(&cam);
 			PROFILE_END();
