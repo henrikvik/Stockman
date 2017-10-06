8ca7eadc090e181f185f3d9f6e0118c9e8be48c9
jakob nyberg <armas.94@hotmail.com>
2017-10-06 13:59:17 +0200

    changed shader to show hp

--- Engine/Resources/Shaders/GUIShader.hlsl
+++ Engine/Resources/Shaders/GUIShader.hlsl
@@ -12,11 +12,28 @@ struct VSOut
     uint elm : ELEMENT;
 };
 
+cbuffer hudInfo : register(b0)
+{
+    float hpProcent;
+    float cdLeft;
+}
+
 VSOut VS(VSIn vsin)
 {
     VSOut vsout;
 
     vsout.pos = float4(vsin.pos, 0, 1);
+    if (vsin.elm == 1)
+    {
+        if (vsin.pos.x > -.9f)
+        {
+            vsin.pos.x -= (abs(-1.0f - vsin.pos.x) * hpProcent);
+        }
+        vsout.pos = float4(vsin.pos, 0, 1);
+    }else
+    {
+        vsout.pos = float4(vsin.pos, 0, 1);
+    }
     vsout.uv = vsin.uv;
     vsout.elm = vsin.elm;
 
@@ -25,6 +42,7 @@ VSOut VS(VSIn vsin)
 
 Texture2D crosshair : register(t0);
 Texture2D HP : register(t1);
+Texture2D outline : register(t2);
 SamplerState sState;
 
 float4 PS(VSOut psin) : SV_Target0
@@ -35,10 +53,13 @@ float4 PS(VSOut psin) : SV_Target0
         return float4(crosshair.Sample(sState, psin.uv));
 
     }
-    else
+    else if (psin.elm == 1)
     {
         //return float4(0.0f, 1.0f, 0.0f, 1.0f);
         return float4(HP.Sample(sState, psin.uv));
+    }else
+    {
+        return float4(outline.Sample(sState, psin.uv));
     }
 
     //return float4(HP.Sample(sState, psin.uv));
