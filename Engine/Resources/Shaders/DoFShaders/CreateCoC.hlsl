995b982b06228fc5da8bad071179060994d4adfe
jakob nyberg <armas.94@hotmail.com>
2017-10-17 16:53:26 +0200

    created the circle of confusion shader

--- Engine/Resources/Shaders/DoFShaders/CreateCoC.hlsl
+++ Engine/Resources/Shaders/DoFShaders/CreateCoC.hlsl
@@ -8,11 +8,21 @@ struct VSOutput
 VSOutput VS( VSInput input)
 {
     VSOutput output = (VSOutput) 0;
+    output.pos = float4(input.pos, 0.0f, 1.0f);
+    output.uv = input.uv;
     return output;
 }
 
+Texture2D colorBuffer : register(t0);
+Texture2D depthBuffer : register(t1);
+
 float4 PS(VSOutput input) : SV_Target0
 {
-    return float4(0.f, 0.f, 0.f, 0.f);
+    float4 color;
+    color.xyz = colorBuffer.Sample(linearSampler, input.uv).xyz;
+    color.w = depthBuffer.Sample(pointSampler, input.uv).x;
 
+    //color.w = 0.5f -((CoCScale * color.w) + CoCBias);
+    color.w = ((CoCScale * color.w) + CoCBias);
+    return color;
 }
