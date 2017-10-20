021c2486030513c9d0052ccfffc5d803fb65c6aa
Andreas Eriksson <dreas94@gmail.com>
2017-10-20 10:34:14 +0200

    Begun the work to set up the command prompt window

--- Engine/DebugWindow.cpp
+++ Engine/DebugWindow.cpp
@@ -1,7 +1,12 @@
 #include "DebugWindow.h"
-
+#define ARRAYSIZE(ARR) ((int)(sizeof(ARR)/sizeof(*ARR)))
 DebugWindow::DebugWindow()
 {
+	clearLog();
+	m_historyPos = -1;
+	m_command.push_back("HELP");
+	m_command.push_back("HISTORY");
+	m_command.push_back("CLEAR");
 }
 
 DebugWindow::~DebugWindow()
@@ -21,6 +26,19 @@ void DebugWindow::clearLog()
 	}
 	m_items.clear();
 	m_scrollToBottom = true;
+	addLog("Welcome to the Debug Window");
+}
+
+void DebugWindow::addLog(const char* command_line, ...) IM_FMTARGS(2)
+{
+	char buf[1024];
+	va_list args;
+	va_start(args, command_line);
+	vsnprintf(buf, ARRAYSIZE(buf), command_line, args);
+	buf[ARRAYSIZE(buf) - 1];
+	va_end(args);
+	m_items.push_back(Strdup(buf));
+	m_scrollToBottom = true;
 }
 
 void DebugWindow::playDebugWindow()
@@ -30,4 +48,8 @@ void DebugWindow::playDebugWindow()
 	{
 		ImGui::End();
 	}
+}
+
+void DebugWindow::doCommand(const char* command_line)
+{
 }
--- Engine/DebugWindow.h
+++ Engine/DebugWindow.h
@@ -3,6 +3,7 @@
 
 #include <imgui.h>
 #include <vector>
+#include <ctype.h>
 
 class DebugWindow
 {
@@ -13,12 +14,43 @@ private:
 	bool						m_scrollToBottom;
 	std::vector<char*>			m_items;
 	std::vector<char*>			m_history;
+	int							m_historyPos;
 	std::vector<const char*>	m_command;
+
+	static int Stricmp(const char* str1, const char* str2) 
+	{
+		int d; 
+		while ((d = toupper(*str2) - toupper(*str1)) == 0 && *str1) 
+		{ 
+			str1++; 
+			str2++; 
+		} 
+		return d; 
+	}
+	static int Strnicmp(const char* str1, const char* str2, int n) 
+	{ 
+		int d = 0; 
+		while (n > 0 && (d = toupper(*str2) - toupper(*str1)) == 0 && *str1) 
+		{ 
+			str1++; 
+			str2++; 
+			n--; 
+		} 
+		return d; 
+	}
+	static char* Strdup(const char *str) 
+	{ 
+		size_t len = strlen(str) + 1;
+		void* buff = malloc(len); 
+		return (char*)memcpy(buff, (const void*)str, len); 
+	}
 
 public:
 	DebugWindow();
 	~DebugWindow();
 	void clearLog();
+	void addLog(const char* command_line, ...) IM_FMTARGS(2);
 	void playDebugWindow();
+	void doCommand(const char* command_line);
 };
 #endif
