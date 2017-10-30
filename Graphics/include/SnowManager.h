c26e991ea5b5239e8433688c3024a7b5efc3465d
Markus Werdien <markuswerdien@hotmail.com>
2017-10-30 11:08:24 +0100

    Only for pull, not working

---
+++ Graphics/include/SnowManager.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Utility\ConstantBuffer.h"
+
+namespace Graphics {
+	class SnowManager
+	{
+	public:
+		SnowManager();
+		~SnowManager();
+
+	private:
+		ConstantBuffer<>
+	};
+}
