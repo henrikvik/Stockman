c7997e7085b3af86dce7460421b65647e96dd240
Fredholm <te11c.simon.fredholm@gmail.com>
2017-11-02 09:52:47 +0100

    Some minor error-handling & a non-copy class

---
+++ Logic/include/Misc/Error.h
@@ -0,0 +1,31 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+// Print & Handle Possible Errors
+#define _SAFE_EXECUTION
+
+#define _ERROR(Result) if (Result != _OK) { printf(GetErrorString(Result)); printf("In File: %s\n Line: %d, Time: %s\n", __FILE__, __LINE__, __TIME__); }
+
+enum _RESULT
+{
+    _OK,
+    _NULLPTR,
+    _INVALID_INDEX, 
+    _FILE_NOT_FOUND
+};
+
+inline char* GetErrorString(_RESULT result)
+{
+    switch (result)
+    {
+    case _OK:                        return "\n- OK.\n"; break;
+    case _NULLPTR:                   return "\n- Invalid pointer was used as a parameter.\n"; break;
+    case _INVALID_INDEX:             return "\n- Index is out of bounds.\n"; break;
+    case _FILE_NOT_FOUND:            return "\n- File not found.\n"; break;
+    default:                        return "\n- Unknown Error.\n"; break;
+    }
+
+    return "Compiler Error?\n";
+}
+
+#endif // !ERROR_H
