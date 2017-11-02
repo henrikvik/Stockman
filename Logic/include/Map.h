a701efed908c4f04c0e3b528694bb0fc1938ca53
Fredholm <te11c.simon.fredholm@gmail.com>
2017-11-02 11:49:18 +0100

    Cleansed Map

--- Logic/include/Map.h
+++ Logic/include/Map.h
@@ -1,55 +1,61 @@
 #ifndef MAP_H
 #define MAP_H
 
+#pragma region Comment
+
+/*
+
+    Description: Container class that carries all non-interactable things on the map
+                    Examples would be: Hitboxes, Visual Models, etc.
+
+                 Classes in this container is not updatable and do not change.
+
+                 Should not be operatated from the outside. 
+                 Only from file.
+
+*/
+
+#pragma endregion Description of class
+
+// Includes
 #include <vector>
-#include <Entity\Object.h>
-#include <Physics\Physics.h>
-#include <Entity\Entity.h>
+#include <Misc\NonCopyable.h>
 #include <Entity/StaticObject.h>
-#include <Misc\MapStructs.h>
 #include <Entity/LightObject.h>
-#include <Misc\FileLoader.h>
+#include <Misc\MapStructs.h>
 
 namespace Logic
 {
-	class Map
+    class StaticObject;
+    class Physics;
+	class Map : public NonCopyable
 	{
 	public:
 		Map();
-		Map(const Map& other) = delete;
-		Map* operator=(const Map& other) = delete;
-		~Map();
-
-        void add(FrameLight frameLight);
-        void add(FrameProp frameProp);
-        void add(FrameHitbox frameHitbox);
+        ~Map();
 
 		void init(Physics* physics);
 		void clear();
-		void update(float deltaTime);
+		void update(float deltaTime);                       //< Can be removed at release, only for toggling debug draw
 		void render(Graphics::Renderer& renderer);
 
 		std::vector<Object*>*				getProps();
 		std::vector<StaticObject*>*			getHitboxes();
-		std::vector<Speaker*>*				getObjects();
         std::vector<LightObject*>*			getLights();
 
 	private:
-		std::vector<Object*>				m_props;
-		std::vector<StaticObject*>			m_hitboxes;
-		std::vector<Speaker*>				m_objects;
-        std::vector<LightObject*>			m_lights;
+		std::vector<Object*>				m_props;        //< Visual objects
+		std::vector<StaticObject*>			m_hitboxes;     //< Non-visual hitboxes
+        std::vector<LightObject*>			m_lights;       //< Pointlights
+        bool                                m_drawDebug;    //< Toggle for debug-draw
+        Physics*                            m_physicsPtr;   //< Ptr to physics for creation of hitboxes
 
-        Physics* m_physicsPtr;
-
-        void readFromFile(std::string path);
-
-        // DEBUG & TESTING STUFF
-        bool m_drawHitboxesAndLights;
-		void debugInitProps();          // move over to readfromfile
-		void debugInitObjects();        // move over to readfromfile
+        // Private functions
+        void add(FrameLight frameLight);
+        void add(FrameProp frameProp);
+        void add(FrameHitbox frameHitbox);
+        void loadMapFromFile(std::string path);
 	};
 }
-
 
 #endif // !MAP_H
