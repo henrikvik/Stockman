616438595e78ddce226fe0aa0540333c4eb58839
bjurrsson <emanuel.bjurman@gmail.com>
2017-11-22 16:48:37 +0100

    bossbaddie score fix

--- Logic/source/AI/EnemyBossBaddie.cpp
+++ Logic/source/AI/EnemyBossBaddie.cpp
@@ -21,6 +21,7 @@ const float EnemyBossBaddie::BASE_SPEED = 21.5f, EnemyBossBaddie::PROJECTILE_SPE
             EnemyBossBaddie::ABILITY_1_MOD = 0.075f, EnemyBossBaddie::MELEE_RANGE = 27.5f,
             EnemyBossBaddie::MELEE_PUSHBACK = 0.3f;
 const int EnemyBossBaddie::BASE_DAMAGE = 1, EnemyBossBaddie::MAX_HP = 300000; // Big guy, for you. well memed
+const int EnemyBossBaddie::SCORE = 10000;
 
 /*
     @author Lukas Westling
