a4fec6c25257a965bebe52043195ec750b58ee0c
Fredholm <te11c.simon.fredholm@gmail.com>
2017-11-23 17:02:00 +0100

    ice buff & bullettime fix

--- Logic/source/Player/Weapon/WeaponManager.cpp
+++ Logic/source/Player/Weapon/WeaponManager.cpp
@@ -207,7 +207,7 @@ void WeaponManager::initializeWeapons(ProjectileManager* projectileManager)
     // **Ice**
     // #######
     /* Primary */       wl.weapon[0] = newd Weapon(projectileManager,
-                                    ProjectileData("Icecone", true, false, 0, 1, 1, 20, 0, 675, Resources::Models::UnitCube, 1, ProjectileType::ProjectileTypeIce, true, false, false),
+                                    ProjectileData("Icecone", true, false, 1, 3, 1, 30, 0, 675, Resources::Models::UnitCube, 1, ProjectileType::ProjectileTypeIce, true, false, false),
                                     Weapon::WeaponInfo{ 2, 1, 17, 5, 750, 0, 1,{ -0.6f, 0.25f, -1.3f } });
     /* Secondary */     wl.weapon[1] = newd WeaponFreezeGrenade(projectileManager,
                                     ProjectileData(nullptr, false, false, 40, 1.f, 1, 100, 0, 5000, Resources::Models::Ammocrystal, 1, ProjectileType::ProjectileTypeFreezeGrenade),
