a2aec6db501664a7e111a776ad2b00090bb05d5e
Lukas Westling <euluken@hotmail.com>
2017-11-27 10:17:53 +0100

    Totem buffs

--- Logic/source/AI/EnemyTotem.cpp
+++ Logic/source/AI/EnemyTotem.cpp
@@ -3,9 +3,9 @@
 #include <Misc\ComboMachine.h>
 using namespace Logic;
 
-const float EnemyTotem::BASE_SPEED = 0.f, EnemyTotem::BULLET_SPEED = 50.f;
-const int EnemyTotem::BASE_DAMAGE = 1, EnemyTotem::MAX_HP = 500, EnemyTotem::SCORE = 25;
-const int EnemyTotem::BULLET_AMOUNT = 8;
+const float EnemyTotem::BASE_SPEED = 0.f, EnemyTotem::BULLET_SPEED = 55.f;
+const int EnemyTotem::BASE_DAMAGE = 1, EnemyTotem::MAX_HP = 550, EnemyTotem::SCORE = 50;
+const int EnemyTotem::BULLET_AMOUNT = 10;
 
 EnemyTotem::EnemyTotem(btRigidBody * body, btVector3 halfExtent)
     : Enemy(Resources::Models::Files::StaticSummon, body, halfExtent, MAX_HP, BASE_DAMAGE, BASE_SPEED, EnemyType::TOTEM, 0, btVector3(0, 0, 0))
@@ -33,8 +33,8 @@ void EnemyTotem::createAbilities()
 
     AbilityData data;
     data.duration = 0.f;
-    data.cooldown = 1000.f;
-    data.randomChanche = 10;
+    data.cooldown = 650.f;
+    data.randomChanche = 7;
 
     spreadShot = Ability(data, [&](Player &target, Ability &ab) -> void {
 
@@ -47,10 +47,10 @@ void EnemyTotem::createAbilities()
         pData.hasEffect = true;
         pData.effectVelocity = false;
         pData.effectActivated = true;
-        m_rotation += 0.5f;
+        m_rotation += 0.25f;
 
         for (int i = 0; i < BULLET_AMOUNT; i++)
-            shoot(btVector3(std::sin((i + m_rotation) * piece), 0.f, std::cos((i + m_rotation) * piece)), pData, BULLET_SPEED, 0.f, 7.f);
+            shoot(btVector3(std::sin((i + m_rotation) * piece), 0.f, std::cos((i + m_rotation) * piece)), pData, BULLET_SPEED, 0.f, 8.5f);
     });
 }
 
