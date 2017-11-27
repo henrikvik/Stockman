cc4871f4f75d8cb6b1fd02e7950ba483611b35b6
Fredholm <te11c.simon.fredholm@gmail.com>
2017-11-27 14:07:31 +0100

    fixed a little crashythingy

--- Logic/source/AI/EnemyNecromancer.cpp
+++ Logic/source/AI/EnemyNecromancer.cpp
@@ -57,7 +57,7 @@ void EnemyNecromancer::createAbilities()
         Projectile *pj = shoot(((player.getPositionBT() - getPositionBT()) + btVector3{ 0, 80, 0 }).normalize(), pdata, (float)SPEED_AB2, 2.5f, 0.6f);
 		if (pj)
 		{
-			pj->addCallback(ON_COLLISION, [&](CallbackData &data) -> void {
+			pj->addCallback(ON_COLLISION, [=](CallbackData &data) -> void {
 				if (m_spawnedMinions < MAX_SPAWNED_MINIONS)
 				{
                     pj->getSoundSource()->playSFX(Sound::SFX::NECROMANCER_SPAWN);
