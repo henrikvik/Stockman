440937389c2b8a6336a0bb024dbebd71e37979f3
Lukas Westling <euluken@hotmail.com>
2017-11-30 15:12:47 +0100

    Fixed

--- Logic/source/StatePlaying.cpp
+++ Logic/source/StatePlaying.cpp
@@ -150,8 +150,6 @@ void StatePlaying::update(float deltaTime)
             }
         }
 
-
-
         PROFILE_BEGIN("Sound");
         Sound::NoiseMachine::Get().update(m_player->getListenerData());
         PROFILE_END();
@@ -176,10 +174,8 @@ void StatePlaying::update(float deltaTime)
         m_projectileManager->update(deltaTime);
         PROFILE_END();
 
-
-
 #define _DEBUG
-#ifdef _DEBUG
+#ifdef  _DEBUG
     if (DirectX::Keyboard::Get().GetState().IsKeyDown(DirectX::Keyboard::NumPad8))
         m_player->takeDamage(1, 0);
 #endif // _DEBUG
