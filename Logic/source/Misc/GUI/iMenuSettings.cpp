f30b21eda791c2d33da4cf772d3c8e830f386d43
Andreas Eriksson <dreas94@gmail.com>
2017-12-01 15:33:48 +0100

    button positions fixed

--- Logic/source/Misc/GUI/iMenuSettings.cpp
+++ Logic/source/Misc/GUI/iMenuSettings.cpp
@@ -7,19 +7,19 @@ Logic::iMenuSettings::iMenuSettings(iMenu::MenuGroup group)
 {
     m_textRenderInfoWindow.color = DirectX::SimpleMath::Color(1, 1, 1, 1);
     m_textRenderInfoWindow.font = Resources::Fonts::KG26;
-    m_textRenderInfoWindow.position = DirectX::SimpleMath::Vector2(815.0f, 130.0f);
+    m_textRenderInfoWindow.position = DirectX::SimpleMath::Vector2(820.0f, 132.5f);
 
     m_textRenderInfoDOF.color = DirectX::SimpleMath::Color(1, 1, 1, 1);
     m_textRenderInfoDOF.font = Resources::Fonts::KG14;
-    m_textRenderInfoDOF.position = DirectX::SimpleMath::Vector2(870.0f, 490.0f);
+    m_textRenderInfoDOF.position = DirectX::SimpleMath::Vector2(870.0f, 495.0f);
 
     m_textRenderInfoSSAO.color = DirectX::SimpleMath::Color(1, 1, 1, 1);
     m_textRenderInfoSSAO.font = Resources::Fonts::KG14;
-    m_textRenderInfoSSAO.position = DirectX::SimpleMath::Vector2(870.0f, 515.0f);
+    m_textRenderInfoSSAO.position = DirectX::SimpleMath::Vector2(870.0f, 520.0f);
 
     m_textRenderInfoFog.color = DirectX::SimpleMath::Color(1, 1, 1, 1);
     m_textRenderInfoFog.font = Resources::Fonts::KG14;
-    m_textRenderInfoFog.position = DirectX::SimpleMath::Vector2(870.0f, 540.0f);
+    m_textRenderInfoFog.position = DirectX::SimpleMath::Vector2(870.0f, 547.0f);
 }
 
 Logic::iMenuSettings::~iMenuSettings()
@@ -33,46 +33,46 @@ void Logic::iMenuSettings::update(int x, int y, float deltaTime)
 
     if (Settings::getInstance().getWindowed() == 1)
     {
-        m_textRenderInfoWindow.text = L"FULL";
-        m_textRenderInfoWindow.position = DirectX::SimpleMath::Vector2(860.0f, 130.0f);
+        m_textRenderInfoWindow.text = L"FULLSCREEN";
+        m_textRenderInfoWindow.position = DirectX::SimpleMath::Vector2(812.0f, 132.5f);
     }
     else
     {
         m_textRenderInfoWindow.text = L"WINDOWED";
-        m_textRenderInfoWindow.position = DirectX::SimpleMath::Vector2(815.0f, 130.0f);
+        m_textRenderInfoWindow.position = DirectX::SimpleMath::Vector2(820.0f, 132.5f);
     }
 
     if (Settings::getInstance().getDOF() == 1)
     {
         m_textRenderInfoDOF.text = L"ON";
-        m_textRenderInfoDOF.position = DirectX::SimpleMath::Vector2(880.0f, 490.0f);
+        m_textRenderInfoDOF.position = DirectX::SimpleMath::Vector2(875.0f, 495.0f);
     }
     else
     {
         m_textRenderInfoDOF.text = L"OFF";
-        m_textRenderInfoDOF.position = DirectX::SimpleMath::Vector2(870.0f, 490.0f);
+        m_textRenderInfoDOF.position = DirectX::SimpleMath::Vector2(870.0f, 495.0f);
     }
 
     if (Settings::getInstance().getSSAO() == 1)
     {
         m_textRenderInfoSSAO.text = L"ON";
-        m_textRenderInfoSSAO.position = DirectX::SimpleMath::Vector2(880.0f, 515.0f);
+        m_textRenderInfoSSAO.position = DirectX::SimpleMath::Vector2(875.0f, 520.0f);
     }
     else
     {
         m_textRenderInfoSSAO.text = L"OFF";
-        m_textRenderInfoSSAO.position = DirectX::SimpleMath::Vector2(870.0f, 515.0f);
+        m_textRenderInfoSSAO.position = DirectX::SimpleMath::Vector2(870.0f, 520.0f);
     }
 
     if (Settings::getInstance().getFog() == 1)
     {
         m_textRenderInfoFog.text = L"ON";
-        m_textRenderInfoFog.position = DirectX::SimpleMath::Vector2(880.0f, 540.0f);
+        m_textRenderInfoFog.position = DirectX::SimpleMath::Vector2(875.0f, 547.0f);
     }   
     else
     {   
         m_textRenderInfoFog.text = L"OFF";
-        m_textRenderInfoFog.position = DirectX::SimpleMath::Vector2(870.0f, 540.0f);
+        m_textRenderInfoFog.position = DirectX::SimpleMath::Vector2(870.0f, 547.0f);
     }
 }
 
