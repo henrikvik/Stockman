1f02d07300df5aa22ad6d3e61d0dabc78aa0bdb1
Fredholm <te11c.simon.fredholm@gmail.com>
2017-12-07 12:45:34 +0100

    asdf

--- Resources/Resources.cpp
+++ Resources/Resources.cpp
@@ -147,7 +147,6 @@ namespace Resources
             if (strcmp(str, "MissingTexture") == 0) return MissingTexture;
             if (strcmp(str, "particle") == 0) return particle;
             if (strcmp(str, "particle_white") == 0) return particle_white;
-            if (strcmp(str, "Pausetext") == 0) return Pausetext;
             if (strcmp(str, "randomNormals") == 0) return randomNormals;
             if (strcmp(str, "Retrytextsheet") == 0) return Retrytextsheet;
             if (strcmp(str, "Settings") == 0) return Settings;
