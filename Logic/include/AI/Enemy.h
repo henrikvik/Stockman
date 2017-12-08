5b4d32e89b2cfe9eae0bcec9473254739002cd2b
Lukas Westling <euluken@hotmail.com>
2017-12-08 09:20:11 +0100

    whops

--- Logic/include/AI/Enemy.h
+++ Logic/include/AI/Enemy.h
@@ -29,7 +29,8 @@ namespace Logic
 	class Enemy : public Entity
 	{
 		private:
-            static const int MIN_Y, MAX_TARGET_Y;
+            static const int MIN_Y;
+            static const float MAX_TARGET_Y;
             // This is used to count how many callbacks is added, 
             // so this HAS to be ZERO before this is destroyed or
             // the showcase at PAX East will go PAX South
