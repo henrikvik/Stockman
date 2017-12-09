db270293bf6542b30dc42922d4a8c9878986184e
Henrik Vik <henkvik@gmail.com>
2017-12-09 23:28:44 +0100

    fixed animated model overriding nested callbacks

--- Logic/include/Entity/AnimatedModel.h
+++ Logic/include/Entity/AnimatedModel.h
@@ -7,34 +7,36 @@
 class AnimatedModel
 {
 public:
-    AnimatedModel(Resources::Models::Files modelId, const char * start_animation);
+    AnimatedModel(Resources::Models::Files model_id, const char * start_animation = "");
 
     void update(float delta_ms);
     void render() const;
 
     void set_color(DirectX::SimpleMath::Vector3 const &color);
-    void set_next(const char * animation, std::function<void(void)> start_callback = nullptr);
+    void set_next(const char * animation, std::function<void(void)> callback = nullptr);
     void set_transform(DirectX::SimpleMath::Matrix &transform);
-    void set_delta_multiplier(float scl);
+    void set_delta_multiplier(float multiplier);
 
     float get_animation_time() const;
+
 private:
     using Matrix = DirectX::SimpleMath::Matrix;
     using Vector3 = DirectX::SimpleMath::Vector3;
 
-    HybrisLoader::Model * model;
-    HybrisLoader::Skeleton * skeleton;
+    NewAnimatedRenderInfo render_info;
 
-    NewAnimatedRenderInfo renderInfo;
+    HybrisLoader::Skeleton * skeleton;
     std::vector<Matrix> joint_transforms;
 
     float frame_progress;
     float frame_duration;
+    float delta_multiplier;
+
     float animation_progress;
     float animation_duration;
-    float delta_scl;
 
     std::string animation_current;
     std::string animation_next;
-    std::function<void(void)> animation_callback;
+
+    std::function<void(void)> callback_next;
 };
--- Logic/source/Entity/AnimatedModel.cpp
+++ Logic/source/Entity/AnimatedModel.cpp
@@ -1,86 +1,92 @@
 #include <Entity/AnimatedModel.h>
 #include <Graphics\include\RenderQueue.h>
 
-AnimatedModel::AnimatedModel(Resources::Models::Files modelId, const char * start_animation)
+AnimatedModel::AnimatedModel(Resources::Models::Files model_id, const char * start_animation)
 {
-    model = ModelLoader::get().getModel(modelId);
+    auto model = ModelLoader::get().getModel(model_id);
     skeleton = &model->getSkeleton();
 
-    renderInfo.model = modelId;
-    renderInfo.color = Vector3(1,1,1);
-    renderInfo.cull_radius = model->get_bounding_box().sphere_radius();
-    renderInfo.useGridTexture = false;
-    renderInfo.joint_transforms = &joint_transforms;
+    render_info.model            = model_id;
+    render_info.color            = Vector3(1,1,1);
+    render_info.cull_radius      = model->get_bounding_box().sphere_radius();
+    render_info.joint_transforms = &joint_transforms;
+    render_info.useGridTexture   = false;
 
     frame_progress = 0.0f;
-    frame_duration = 1.0f / 30.0f;
+    frame_duration = 1.0f / 60.0f;
+    delta_multiplier = 1.f;
 
-    animation_next     = start_animation;
+    animation_progress = 0.0f;
+    animation_duration = 0.0f;
     animation_current  = "";
-    animation_duration = 0;
+    animation_next     = start_animation;
 
-    delta_scl = 1.f;
+    callback_next    = nullptr;  
 }
 
 void AnimatedModel::update(float delta_ms)
 {
-    frame_progress += delta_ms * delta_scl / 1000.0f;
+    static float to_seconds = 1.0f / 1000.0f;
+    frame_progress += delta_ms * delta_multiplier * to_seconds;
 
+    // early return if its not time to update the animation
     if (frame_progress < frame_duration) return;
 
     frame_progress     -= frame_duration;
     animation_progress += frame_duration;
 
+    // check if there is an animation to update
     if (!animation_current.empty())
     { 
-        joint_transforms = 
-            skeleton->evalAnimation(
-                animation_current.c_str(),
-                animation_progress
-            );
+        joint_transforms = skeleton->evalAnimation(
+            animation_current.c_str(),
+            animation_progress
+        );
     }
 
+    // early return if the animation has not finished
     if (animation_progress < animation_duration) return;
 
     frame_progress     = 0.0f;
     animation_progress = 0.0f;
 
+    // early return if there is no animation queued
     if (animation_next.empty()) return;
 
     animation_current  = animation_next;
     animation_duration = skeleton->getAnimationDuration(animation_next.c_str());    
     animation_next     = "";
 
-    if (animation_callback == nullptr) return;
-    
-    animation_callback();
-    animation_callback = nullptr;
+    // early return if the animation queded had no callback
+    if (callback_next == nullptr) return;
+
+    callback_next();
 }
 
 void AnimatedModel::render() const
 {
-    QueueRender(renderInfo);
+    QueueRender(render_info);
 }
 
 void AnimatedModel::set_color(DirectX::SimpleMath::Vector3 const &color)
 {
-    renderInfo.color = color;
+    render_info.color = color;
 }
 
-void AnimatedModel::set_next(const char *animation, std::function<void(void)> start_callback)
+void AnimatedModel::set_next(const char *animation, std::function<void(void)> callback)
 {
     animation_next = animation;
-    animation_callback = start_callback;
+    callback_next  = callback;
 }
 
 void AnimatedModel::set_transform(DirectX::SimpleMath::Matrix &transform)
 {
-    renderInfo.transform = transform;
+    render_info.transform = transform;
 }
 
-void AnimatedModel::set_delta_multiplier(float scl)
+void AnimatedModel::set_delta_multiplier(float multiplier)
 {
-    this->delta_scl = scl;
+    this->delta_multiplier = multiplier;
 }
 
 float AnimatedModel::get_animation_time() const
