2d53e4fc21615f51296532037000a677936c8fa7
Fredholm <te11c.simon.fredholm@gmail.com>
2017-12-12 12:52:56 +0100

    Hide Windows console, commented out currently, uncomment on release and change SYSTEM settings in the engine settings

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -9,8 +9,12 @@
 #include <stdlib.h>  
 #include <crtdbg.h>
 
-// int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+// For release
+// #if _DEBUG 
 int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+// #else
+// int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+// #endif
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     int r = 0;
