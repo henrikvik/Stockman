e046ccac88976e4f234c3c29536b6db6c6e87fa9
bjurrsson <emanuel.bjurman@gmail.com>
2017-12-13 13:32:03 +0100

    fixed noclip command hopefully

--- Logic/source/Player/Player.cpp
+++ Logic/source/Player/Player.cpp
@@ -158,10 +158,7 @@ void Player::registerDebugCmds()
 
     win->registerCommand("LOG_NOCLIP", [&](std::vector<std::string> &para) -> std::string {
         m_noclip = !m_noclip;
-        if (m_noclip)
-            m_charController->setGravity({ 0.f, 0.f, 0.f }); // remove gravity
-        else {
-            m_charController->setGravity({ 0.f, -PLAYER_GRAVITY, 0.f });
+        if (!m_noclip) {
             // reset movement
             m_moveDir.setZero();
             m_moveSpeed = 0.f;
@@ -321,6 +318,11 @@ void Player::reset()
     m_damageTintTimer = 0.f;
     m_upgradeTintTimer = 0.f;
     m_pickupTintTimer = 0.f;
+    
+    m_noclip = false;
+    m_godMode = false;
+
+    resetTargeted();
 
     getStatusManager().clear();
     getStatusManager().addUpgrade(StatusManager::FIRE_UPGRADE);
