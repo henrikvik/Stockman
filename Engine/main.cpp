1e08ad79190de2130cd51a2efd3ef21a35ad213b
Henrik Vik <henkvik@gmail.com>
2017-12-15 17:16:06 +0100

    release is singel window

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -10,11 +10,11 @@
 #include <crtdbg.h>
 
 // For release
-// #if _DEBUG 
-//int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
-// #else
- int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
-// #endif
+#if _DEBUG 
+int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+#else
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+#endif
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
     int r = 0;
